7dc745ee947415b9fe8943ec67c737f537670ed3
Kellen Green <me@kellengreen.com>
2014-03-13 23:47:41 -0700

    wip

--- README.md
+++
@@ -1,4 +0,0 @@
-wedding
-=======
-
-Wedding Site
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,5 +1,28 @@
-var app = angular.module('weddingApp', []);
+console.info('G.O.N.Z.A.G.A!');
+
+var app = angular.module('weddingApp', ['ngRoute']);
+
+//app.config(function($routeProvider, $locationProvider) {
+//
+//    $routeProvider.when('/about', {
+//        controller: 'weddingCtrl'
+//    });
+//
+//    $locationProvider.html5Mode(false);
+//
+//});
 
 app.controller('weddingCtrl', function ($scope) {
 
+    $scope.viewstate;
+
+    $scope.viewChange = function(state) {
+        if ($scope.viewState === state) {
+            $scope.viewState = undefined;
+            window.location.hash = '';
+        } else {
+            $scope.viewState = state;
+            window.location.hash = '#/' + state;
+        }
+    }
 });
