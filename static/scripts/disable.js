d39d2431d3991be5647793f3065756979131b492
Kellen Green <me@kellengreen.com>
2014-03-27 02:01:00 -0700

    disable typo

--- static/scripts/disable.js
+++ static/scripts/disable.js
@@ -1,3 +1,3 @@
-(function(){var b=document.body;if(b.style.flex!=undefined){
+(function(){var b=document.body;if(b.style.flex==undefined){
 b.innerHTML='<h4 class="text-center">Gonzaga had a football team when this browser came out, \
 time to <a target="_blank" href="https://www.google.com/chrome/">upgrade</a>!</h4>'}})();
